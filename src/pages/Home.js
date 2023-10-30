757f31abd475aa9d57f89211da4bd9a26f2958a9
Adam <N00211418@iadt.ie>
2023-10-30 14:19:50 +0000

    added search mvp

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,15 +6,27 @@ import { Row } from 'react-bootstrap'
 
 // components
 import CountryCard from '../components/CountryCard'
+import SearchBar from '../components/SearchBar'
 
 const Home = () => {
 
 	const [countriesArray, setCountriesArray] = useState([])
+	const [searchValue, setSearchValue] = useState('')
 	const ALL_COUNTRIES_API = 'https://restcountries.com/v3.1/all'
+	const SEARCH_COUNTRIES_API = `https://restcountries.com/v3.1/name/`
+
 
 	useEffect(() => {
-		axios.get(ALL_COUNTRIES_API)
+		getEveryCountry()
+	}, [])
 
+	useEffect(() => {
+		searchCountry()
+	},[searchValue])
+
+
+	const searchCountry = () => {
+		axios.get(SEARCH_COUNTRIES_API + searchValue)
 			.then(response => {
 				setCountriesArray(response.data)
 			})
@@ -22,7 +34,24 @@ const Home = () => {
 			.catch(error => {
 				console.error(error)
 			})
-	}, [])
+	}
+
+	const getEveryCountry = () => {
+		axios.get(ALL_COUNTRIES_API, {
+			fullText: true
+		})
+			.then(response => {
+				setCountriesArray(response.data)
+			})
+
+			.catch(error => {
+				console.error(error)
+			})
+	}
+
+	const handleSearchClick = (searchTerm) => {
+		setSearchValue(searchTerm)
+	}
 
 	let countriesCards = countriesArray.map((country, index) => {
 		return <CountryCard key={index} country={country} />
@@ -31,6 +60,7 @@ const Home = () => {
 	return (
 		<>
 			<div>Home</div>
+			<SearchBar handleSearchClick={handleSearchClick} />
 			<Row className='g-4' md={3} xs={1}>
 				{countriesCards}
 			</Row>
