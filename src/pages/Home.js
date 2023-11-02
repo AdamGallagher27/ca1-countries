5cee9015e78e4ea04dcfa114a0fadc185d2114ec
Adam Gallagher <n00211418@iadt.ie>
2023-11-02 14:04:04 +0000

    started sort

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,7 @@ import { Row, Button } from 'react-bootstrap'
 // components
 import CountryCard from '../components/CountryCard'
 import SearchBar from '../components/SearchBar'
+import SortDropDown from '../components/SortDropDown'
 
 
 const Home = () => {
@@ -14,12 +15,17 @@ const Home = () => {
 	const [countriesArray, setCountriesArray] = useState([])
 	const [filteredCountries, setFilteredCountries] = useState([])
 	const [searchValue, setSearchValue] = useState('')
+	const [sort, setSort] = useState('')
 	const [paginationLimit, setPaginationLimit] = useState(15)
 	const ALL_COUNTRIES_API = 'https://restcountries.com/v3.1/all'
-	
+
 	useEffect(() => {
 		getEveryCountry()
 	}, [])
+
+	useEffect(() => {
+		sortCountryCards()
+	}, [sort])
 
 
 	const getEveryCountry = () => {
@@ -57,22 +63,56 @@ const Home = () => {
 		setPaginationLimit(paginationLimit + 9)
 	}
 
+	const dispatchSortValue = (sortValue) => {
+		setSort(sortValue)
+	}
+
+	const sortCountryCards = () => {
+		switch (sort) {
+			case 'alphabetical':
+				console.log('alphabetical')
+
+				// const sortAlphabetically = filteredCountries.sort((a, b) => a.name.common > b.name.common)
+
+				// console.log(filteredCountries)
+				// console.log(sortAlphabetically)
+				break
+
+			case 'population':
+				console.log('population')
+
+				// const sortPopulation = filteredCountries.sort((a, b) => a.population - b.population)
+				// setFilteredCountries(sortPopulation)
+
+				// console.log(filteredCountries)
+				// console.log(sortPopulation)
+				break
+
+			case 'area':
+				console.log('area')
+				break
+		}
+	}
+
+
 
 	let countriesCards = filteredCountries.map((country, index) => {
-		if(index < paginationLimit) {
+		if (index < paginationLimit) {
 			return <CountryCard key={index} country={country} />
 		}
 	})
 
+
 	return (
 		<>
 			<SearchBar handleSearch={handleSearch} />
+			<SortDropDown dispatchSortValue={dispatchSortValue} />
 			<Row className='g-4' md={3} xs={1}>
 				{/* MAKE ME INTO SEARCH ERROR COMPONENT */}
 				{countriesCards.length > 0 ? countriesCards : `sorry we couldnt find anything for ${searchValue}`}
 			</Row>
-				{/* MAKE PAGINATE BUTTON COMPONENT */}
-				<Button onClick={loadMoreCountries}>Pagination</Button>
+			{/* MAKE PAGINATE BUTTON COMPONENT */}
+			<Button onClick={loadMoreCountries}>Pagination</Button>
 
 		</>
 	)
