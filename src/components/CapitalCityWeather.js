1682571a55f5d1e7dd5b79ab2286a2ed3970e94f
Adam Gallagher <n00211418@iadt.ie>
2023-11-02 19:18:07 +0000

    created component for weather

---
+++ src/components/CapitalCityWeather.js
@@ -0,0 +1,28 @@
+import {Card} from 'react-bootstrap';
+
+const CapitalCityWeather = (props) => {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Todays Weather in {props.city}</Card.Title>
+        <Card.Text>
+          Humidity Levels : {props.weather.humidity} RH
+        </Card.Text>
+        <Card.Text>
+          Current Tempature : {props.weather.temp} °
+        </Card.Text>
+        <Card.Text>
+          Todays Minimum Tempature : {props.weather.min_temp} °
+        </Card.Text>
+        <Card.Text>
+          Todays Max Tempature : {props.weather.max_temp} °
+        </Card.Text>
+        <Card.Text>
+    	    Wind Speed : {props.weather.wind_speed} KM/H
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  )
+}
+
+export default CapitalCityWeather
