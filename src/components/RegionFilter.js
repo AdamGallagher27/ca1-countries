0f59cd73b9ce5301eb7b5394665cae25f8508ec7
Adam <N00211418@iadt.ie>
2023-11-04 20:13:10 +0000

    created basic filter by region

---
+++ src/components/RegionFilter.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Dropdown, DropdownButton } from 'react-bootstrap'
+
+
+const RegionFilter = (props) => {
+
+  const handleChange = (eventKey) => {
+    props.dispatchRegionValue(eventKey)
+  }
+
+  return (
+    <DropdownButton title='regionFilter' onSelect={handleChange}>
+      <Dropdown.Item eventKey="default">Default</Dropdown.Item>
+      <Dropdown.Item eventKey="europe">Europe</Dropdown.Item>
+      <Dropdown.Item eventKey='africa'>Africa</Dropdown.Item>
+      <Dropdown.Item eventKey='asia'>Asia</Dropdown.Item>
+      <Dropdown.Item eventKey='oceania'>Oceania</Dropdown.Item>
+      <Dropdown.Item eventKey='americas'>Americas</Dropdown.Item>
+    </DropdownButton>
+  )
+}
+
+export default RegionFilter
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,36 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-import { Row, Button } from "react-bootstrap";
+import { useState, useEffect } from "react"
+import axios from "axios"
+import { Row, Button, Col } from "react-bootstrap"
 
 // components
-import CountryCard from "../components/CountryCard";
-import SearchBar from "../components/SearchBar";
-import SortDropDown from "../components/SortDropDown";
+import CountryCard from "../components/CountryCard"
+import SearchBar from "../components/SearchBar"
+import SortDropDown from "../components/SortDropDown"
+import RegionFilter from "../components/RegionFilter"
 
 const Home = () => {
-	const [startingCountries, setStartingCountries] = useState([]);
-	const [filteredCountries, setFilteredCountries] = useState([]);
-	const [searchValue, setSearchValue] = useState("");
-	const [sortType, setSortType] = useState("");
-	const [paginationLimit, setPaginationLimit] = useState(15);
-	const ALL_COUNTRIES_API = "https://restcountries.com/v3.1/all";
+	const [startingCountries, setStartingCountries] = useState([])
+	const [filteredCountries, setFilteredCountries] = useState([])
+	const [searchValue, setSearchValue] = useState("")
+	const [sortType, setSortType] = useState("")
+	const [region, setRegion] = useState("")
+	const [paginationLimit, setPaginationLimit] = useState(15)
+	const ALL_COUNTRIES_API = "https://restcountries.com/v3.1/all"
 
 	// on first load get every country
 	useEffect(() => {
-		getEveryCountry();
-	}, []);
+		getEveryCountry()
+	}, [])
 
 	// if the sort type is updated sort the country cards
 	useEffect(() => {
-		sortCountryCards();
-	}, [sortType]);
+		sortCountryCards()
+	}, [sortType])
+
+	// if the region value is updated filter the country cards
+	useEffect(() => {
+		applyRegionFilter(region)
+	}, [region])
 
 	// return every country from api
 	const getEveryCountry = () => {
@@ -34,122 +41,155 @@ const Home = () => {
 
 			// assign response to the starting countries array and the filtered countries array
 			.then((response) => {
-				setStartingCountries(response.data);
-				setFilteredCountries(response.data);
+				setStartingCountries(response.data)
+				setFilteredCountries(response.data)
 			})
 
 			.catch((error) => {
-				console.error(error);
-			});
-	};
+				console.error(error)
+			})
+	}
 
 	// get search from search box and apply filter to filtered countries array
 	const handleSearch = (searchTerm) => {
 		// set the search value
-		setSearchValue(searchTerm);
+		setSearchValue(searchTerm)
 
 		// reset the pagination
-		resetPagination();
+		resetPagination()
 
 		// create the filter to apply to filtered countries
 		const filter = startingCountries.filter((country) => {
 			return country.name.common
 				.toLowerCase()
-				.includes(searchTerm.toLowerCase());
-		});
+				.includes(searchTerm.toLowerCase())
+		})
 
 		// apply filter
-		setFilteredCountries(filter);
-	};
+		setFilteredCountries(filter)
+	}
 
 	// restore pagination to original 15 cards
 	const resetPagination = () => {
-		setPaginationLimit(15);
-	};
+		setPaginationLimit(15)
+	}
 
 	// load more country cards
 	const loadMoreCountries = () => {
-		setPaginationLimit(paginationLimit + 9);
-	};
+		setPaginationLimit(paginationLimit + 9)
+	}
 
 	// function to change the sort value from sort dropdown component
 	const dispatchSortValue = (sortValue) => {
-		setSortType(sortValue);
-	};
+		setSortType(sortValue)
+	}
+
+	const dispatchRegionValue = (selectedRegion) => {
+		setRegion(selectedRegion)
+		console.log(selectedRegion)
+	}
 
 	// reset the cards to default order
 	const restoreDefaultOrder = () => {
-		setFilteredCountries(startingCountries);
-	};
+		setFilteredCountries(startingCountries)
+		resetPagination()
+	}
 
 	// sort the countries by alphabetical order
 	const sortByAlphabetical = () => {
-		const sortAlphabetically = [...filteredCountries];
+		const sortAlphabetically = [...filteredCountries]
 		sortAlphabetically.sort((a, b) => {
 			if (a.name.common < b.name.common) {
-				return -1;
+				return -1
 			} else if (b.name.common < a.name.common) {
-				return 1;
+				return 1
 			} else {
-				return 0;
+				return 0
 			}
-		});
+		})
 
-		setFilteredCountries(sortAlphabetically);
-	};
+		setFilteredCountries(sortAlphabetically)
+		resetPagination()
+	}
 
 	// sort the countries by population
 	const sortByPopulation = () => {
-		const sortByPopulation = [...filteredCountries];
-		sortByPopulation.sort((a, b) => b.population - a.population);
-		setFilteredCountries(sortByPopulation);
-	};
+		const sortByPopulation = [...filteredCountries]
+		sortByPopulation.sort((a, b) => b.population - a.population)
+		setFilteredCountries(sortByPopulation)
+		resetPagination()
+	}
 
 	// sort the countries by area size
 	const sortByArea = () => {
-		const sortByArea = [...filteredCountries];
-		sortByArea.sort((a, b) => b.area - a.area);
-		setFilteredCountries(sortByArea);
-	};
+		const sortByArea = [...filteredCountries]
+		sortByArea.sort((a, b) => b.area - a.area)
+		setFilteredCountries(sortByArea)
+		resetPagination()
+	}
 
 	// change the order of the country cards based on sort dropdown
 	const sortCountryCards = () => {
 		switch (sortType) {
 			case "default":
-				restoreDefaultOrder();
-				break;
+				restoreDefaultOrder()
+				break
 
 			case "alphabetical":
-				sortByAlphabetical();
-				break;
+				sortByAlphabetical()
+				break
 
 			case "population":
-				sortByPopulation();
-				break;
+				sortByPopulation()
+				break
 
 			case "area":
-				sortByArea();
-				break;
+				sortByArea()
+				break
 		}
-	};
+	}
+
+	const applyRegionFilter = (selectedRegion) => {
+		if (selectedRegion === 'default') {
+			setFilteredCountries(startingCountries)
+		}
+		else {
+			const filteredCountriesCopy = [...filteredCountries]
+			
+			const countriesByRegion = filteredCountriesCopy.filter((country) => {
+				return (country.region.toLowerCase() === selectedRegion)
+			})
+
+			setFilteredCountries(countriesByRegion)
+		}
+
+		resetPagination()
+	}
 
 	// create card components
 	const createCardComponents = () => {
 		return filteredCountries.map((country, index) => {
 			// apply pagination limit
 			if (index < paginationLimit) {
-				return <CountryCard key={index} country={country} />;
+				return <CountryCard key={index} country={country} />
 			}
-		});
-	};
+		})
+	}
 
 	// variable to hold countries cards
-	const countriesCards = createCardComponents();
+	const countriesCards = createCardComponents()
 
 	return (
 		<>
 			<SearchBar handleSearch={handleSearch} />
-			<SortDropDown dispatchSortValue={dispatchSortValue} />
+			<Row>
+				<Col>
+					<SortDropDown dispatchSortValue={dispatchSortValue} />
+				</Col>
+				<Col>
+					<RegionFilter dispatchRegionValue={dispatchRegionValue} />
+				</Col>
+			</Row>
 			<Row className="g-4" md={3} xs={1}>
 				{/* MAKE ME INTO SEARCH ERROR COMPONENT */}
 				{countriesCards.length > 0
@@ -159,7 +199,7 @@ const Home = () => {
 			{/* MAKE PAGINATE BUTTON COMPONENT */}
 			<Button onClick={loadMoreCountries}>Pagination</Button>
 		</>
-	);
-};
+	)
+}
 
-export default Home;
+export default Home
