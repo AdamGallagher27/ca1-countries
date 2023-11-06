11fd3ccb4fe9af218ddaf61c5c5bdef033909754
Adam <N00211418@iadt.ie>
2023-11-06 18:27:57 +0000

    created search error

---
+++ src/components/SearchError.js
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const SearchError = (props) => {
+  return (
+    <p>sorry we couldnt find anything for '{props.searchValue}'</p>
+  )
+}
+
+export default SearchError
