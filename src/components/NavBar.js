be61a5d7782d854f2f95e5cb1cf03a69348b2637
Adam <N00211418@iadt.ie>
2023-11-08 01:14:36 +0000

    added region route

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,14 +12,22 @@ const NavBar = () => {
     navigate('/')
   }
 
+  const navigateToContinentSelect = () => {
+    navigate('/continents')
+  }
+
+
   return (
-      <Navbar className=" mb-5 border-bottom bg-success
+    <Navbar className=" mb-5 border-bottom bg-success
       ">
-        <Container>
-              <Navbar.Brand className='text-light'>Country DataBase</Navbar.Brand>
-              <Button className='btn-light' onClick={navigateToHome}>HomePage</Button>
-        </Container>
-      </Navbar>
+      <Container>
+        <Navbar.Brand className='text-light'>Country DataBase</Navbar.Brand>
+        <div>
+          <Button className='btn-light mx-4' onClick={navigateToContinentSelect}>Continents</Button>
+          <Button className='btn-light' onClick={navigateToHome}>HomePage</Button>
+        </div>
+      </Container>
+    </Navbar>
   )
 }
 
