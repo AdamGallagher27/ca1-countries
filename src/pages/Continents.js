5e47dddd4757c8e335959f4a94f62b3373b39a41
Adam <N00211418@iadt.ie>
2023-11-08 19:35:30 +0000

    added search functionality

--- src/pages/Continents.js
+++ src/pages/Continents.js
@@ -1,3 +1,5 @@
+
+// hooks
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
@@ -7,19 +9,25 @@ import SortDropDown from '../components/SortDropDown'
 import Loading from '../components/Loading'
 import SearchError from '../components/SearchError'
 import PaginateButton from '../components/PaginateButton'
+import SearchBar from '../components/SearchBar'
+import Title from '../components/Title'
 
 // utilities
 import { getDataByRegion } from '../utilities/API'
 import { sortCountryCards } from '../utilities/sort'
 import { createCardComponents } from '../utilities/createCards'
+import { applySearchFilter } from '../utilities/applySearchFilter'
+
 
 const Continents = () => {
 
   const [countries, setCountries] = useState([])
+  const [startingCountries, setStartingCountries] = useState([])
   const [paginationLimit, setPaginationLimit] = useState([15])
   const [sortType, setSortType] = useState("")
   const [searchValue, setSearchValue] = useState('')
 
+  // get name of continent from route param
   const { continentName } = useParams()
 
   useEffect(() => {
@@ -28,6 +36,7 @@ const Continents = () => {
 
   useEffect(() => {
     applySort()
+    resetPagination()
   }, [sortType])
 
   // get countries data helper function
@@ -35,6 +44,7 @@ const Continents = () => {
     try {
       const countriesData = await getDataByRegion(continentName)
       setCountries(countriesData)
+      setStartingCountries(countriesData)
     }
     catch (error) {
       console.error(error)
@@ -52,6 +62,11 @@ const Continents = () => {
     setSortType(sortValue)
   }
 
+  // reset the sort value
+  const resetSort = () => {
+    setSortType('none')
+  }
+
 	// restore pagination to original 15 cards
 	const resetPagination = () => {
 		setPaginationLimit(15)
@@ -59,19 +74,38 @@ const Continents = () => {
 
 	// load more country cards
 	const loadMoreCountries = () => {
-		setPaginationLimit(paginationLimit + 1)
+		setPaginationLimit(paginationLimit + 9)
 	}
+
+  // get search value and apply it to countries array
+  const handleSearch = (searchTerm) => {
+
+    // reset the pagination, sort value and region
+    resetPagination()
+    resetSort()
+
+    // set search value
+    setSearchValue(searchTerm)
+
+    // create search filter and apply it to the countries array
+    const searchFilter = applySearchFilter(searchTerm, startingCountries)
+
+    // set countries array
+    setCountries(searchFilter)
+  }
 
   // if countries is not set return loading component
 	if(!countries) {
 		return <Loading />
 	}
 
+  // create countries cards
   const countriesCards = createCardComponents(countries, paginationLimit)
 
   return (
     <>
-      <h1 className='mb-4'>{continentName}</h1>
+      <Title title={continentName} />
+      <SearchBar handleSearch={handleSearch} />
       <div className="d-flex flex-row gap-3 mb-4">
         <SortDropDown dispatchSortValue={dispatchSortValue} />
       </div>
